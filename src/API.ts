70aac281bec60c1b8d48287eb46abfd8cd896aec
Alikhan219 <merabhai786786@gmail.com>
2020-11-29 10:12:43 -0800

    specifying  the type for each question

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,19 @@
 
+export type Question=
+{
+    category: string;
+    correct_answer:string;
+    difficulty: string;
+    incorrect_answer: string[];
+    question: string;
+    type: string;
+
+}
+
+export type QuestionState= Question & {answers: string[]};
+
+
+
 export enum Difficulty {
     EASY = "easy",
     MEDIUM= "medium",
@@ -8,5 +23,10 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) =>{
     const endpoint= `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data= await (await fetch(endpoint)).json();
-    console.log(data)
+    return data.rsults.map((question:Question)=>(
+        {
+            ...question,
+            answer: 
+        }
+    ))
 }
