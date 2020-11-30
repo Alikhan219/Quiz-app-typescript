cc78b633ce7722b8c1c45c63b55281ccc99c7749
Alikhan219 <merabhai786786@gmail.com>
2020-11-30 08:42:02 -0800

    importing the shufulArray function in the api.ts for shuffling of array

--- src/API.ts
+++ src/API.ts
@@ -1,10 +1,12 @@
+import {shuffleArray} from "./utils";
+
 
 export type Question=
 {
     category: string;
     correct_answer:string;
     difficulty: string;
-    incorrect_answer: string[];
+    incorrect_answers: string[];
     question: string;
     type: string;
 
@@ -23,10 +25,11 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) =>{
     const endpoint= `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data= await (await fetch(endpoint)).json();
-    return data.rsults.map((question:Question)=>(
+    return data.results.map((question:Question)=>(
         {
             ...question,
-            answer:  
+            answer: shuffleArray([
+                ...question.incorrect_answers, question.correct_answer])
         }
     ))
 }
