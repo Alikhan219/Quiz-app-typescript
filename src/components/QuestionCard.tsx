67c71a5082c5be597516c62052656cfc190616c9
Alikhan219 <merabhai786786@gmail.com>
2020-12-07 03:54:57 -0800

    exporting the AnswerObject from app.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {AnswerObject} from "../App"
 type props ={
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>)=> void;
+    userAnswer: AnswerObject / undefined;
     questionNr: number;
     totalQuestions: number;
 }
@@ -24,7 +25,7 @@ const QuestionCard: React.FC<props> =({
             {console.log("answers: ",answers)}
               {answers?.map(answer =>(
                   <div key={answer}>
-                      <button disabled={userAnswer}  
+                      <button disabled={userAnswer ? true : false}  
                       value={answer}
                       onClick={callback}>
           <span dangerouslySetInnerHTML={{__html: answer}} />
