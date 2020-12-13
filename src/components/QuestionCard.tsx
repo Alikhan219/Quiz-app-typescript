039734312afba92f6a68de72b5d36a88a5ba4165
Alikhan219 <merabhai786786@gmail.com>
2020-12-13 09:07:04 -0800

    giving styles to the questioncard

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {AnswerObject} from "../App"
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 type props ={
     question: string;
     answers: string[];
@@ -16,25 +17,25 @@ const QuestionCard: React.FC<props> =({
     questionNr,
     totalQuestions})=> (
 
-    <div>
-        <p className="number">
-            Question: {questionNr} / {totalQuestions}
-        </p> 
-        <p dangerouslySetInnerHTML={{__html: question}} />
+        <Wrapper>
+        <p className='number'>
+          Question: {questionNr} / {totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: question }} />
         <div>
-            {console.log("answers: ",answers)}
-              {answers?.map(answer =>(
-                  <div key={answer}>
-                      <button disabled={userAnswer ? true : false}  
-                      value={answer}
-                      onClick={callback}>
-          <span dangerouslySetInnerHTML={{__html: answer}} />
-                      </button>
-                  </div>
-
-              ))}
+          {answers.map((answer) => (
+            <ButtonWrapper
+              key={answer}
+              correct={userAnswer?.correctAnswer === answer}
+              userClicked={userAnswer?.answer === answer}
+            >
+              <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+                <span dangerouslySetInnerHTML={{ __html: answer }} />
+              </button>
+            </ButtonWrapper>
+          ))}
         </div>
-    </div>
+      </Wrapper>
 
     )
 
